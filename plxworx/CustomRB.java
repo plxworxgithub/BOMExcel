e3cab80bc88b895ede2fbde4d36806c9ae30d029
Admin <testsmtp2023@outlook.com>
2024-04-17 12:51:19 +0530

    RB file

---
+++ plxworx/CustomRB.java
@@ -0,0 +1,23 @@
+
+
+package ext.plxworx;
+import wt.util.resource.RBEntry;
+import wt.util.resource.RBUUID;
+import wt.util.resource.WTListResourceBundle;
+
+@RBUUID("ext.plxworx.CustomRB")
+public class CustomRB extends WTListResourceBundle {
+    @RBEntry("Custom Action")
+    public static final String PRIVATE_CONSTANT_0 = "object.customtable.description";
+
+    @RBEntry("Custom Action")
+    public static final String PRIVATE_CONSTANT_1 = "object.customtable.tooltip";
+
+    @RBEntry("Custom Action")
+    public static final String PRIVATE_CONSTANT_2 = "object.customtable.title";
+
+    @RBEntry("newpart.gif")
+    public static final String PRIVATE_CONSTANT_3 = "object.customtable.icon";
+
+}
+
